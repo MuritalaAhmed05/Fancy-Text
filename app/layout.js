4d669162e197d9070bacb87fd78458fbb21b6344
MuritalaAhmed <muritalaahmed405@gmail.com>
2024-09-03 01:01:07 +0100

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Tangerine"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
